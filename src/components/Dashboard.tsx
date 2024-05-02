1fc939432aeae5e295d1fcc871f839d61572aad1
kadeeja <kadeejagriffin@yahoo.com>
2024-05-02 15:37:07 -0400

    actually updated learn button

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -122,36 +122,6 @@ const DashboardPage: React.FC = () => {
                                 Categories
                             </Link>
                             {/* Learn Button */}
-                                <button className="block-flex py-2 flex items-center" onClick={toggleDropdown}>
-                                    <svg
-                                        className="h-8 w-8 text-black-500 inline-block mr-2"
-                                        width="24"
-                                        height="24"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth="2"
-                                        stroke="currentColor"
-                                        fill="none"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
-                                    </svg>
-                                    Learn
-                                </button>
-                                {/* Dropdown */}
-                                {showDropdown && (
-                                    <div className="absolute mt-2 py-2 w-40 bg-white border rounded-lg shadow-md">
-                                        <Link to="/investment_regiment" className="block px-4 py-2 hover:bg-gray-100">
-                                            Investments & Retirement
-                                        </Link>
-                                        <Link to="/loans&mortgage" className="block px-4 py-2 hover:bg-gray-100">
-                                            Loans & Mortgages
-                                        </Link>
-                                        <Link to="/credit" className="block px-4 py-2 hover:bg-gray-100">
-                                            Building Credit
-                                        </Link>
-                                    </div>
-                                )}
                             <button className="block py-2 items-center" onClick={toggleDropdown}>
                                 <svg
                                     className="h-8 w-8 text-black-500 inline-block mr-2"
